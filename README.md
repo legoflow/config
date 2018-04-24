2b0b026c3bf123c9a7995d46a43e9cf375927273
lijialiang <lijialiang@yy.com>
2018-04-24 15:19:18 +0800

    [update] v0.0.2

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# config
-the global config
+<h1 align="center"> Config </h1>
+
+<p align="center">
+    <a href="https://opensource.org/licenses/MIT">
+        <img alt="Licence" src="https://img.shields.io/badge/license-MIT-green.svg" />
+    </a>
+    <a href="">
+        <img alt="PRs Welcome" src="https://img.shields.io/badge/PRs-welcome-green.svg" />
+    </a>
+</p>
+
+<p align="center">
+    <strong>全局配置</strong>
+</p>
+
+## 参数配置
+
+名称 | 类型 | 默认值 | 备注
+-----|------- | -----|-------
+user | String | null | 用户名
+port | Number | 3000 | 开发工作流端口号
+editor | String | VSCode | 常用编辑器，可选值 VSCode / Atom / sublimeText3 / WebStorm
+autoOpenChrome | Boolean | true | 工作流是否自动打开 chrome
+lab | Boolean | false | 是否打开 实验性功能 |
+NodeBin | String | null | Shell 模块的扩展（ [详情](https://github.com/legoflow/legoflow/wiki/9.5.-%E5%A6%82%E4%BD%95%E7%90%86%E8%A7%A3-node-bin) ） |
+
+## 许可
+
+[MIT](./LICENSE)
---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+
+const userRoot = os.homedir( );
+
+const globalConfigPath = path.resolve( userRoot, './.legoflow_config' );
+
+const defaultConfig = {
+    user: '',
+    autoOpenChrome: true,
+    port: 3000,
+    editor: 'VSCode',
+    lab: false,
+    nodeBin: '',
+};
+
+let config = defaultConfig;
+
+const writeConfig = ( ) => {
+    fs.writeFileSync( globalConfigPath, JSON.stringify( config ), 'utf8' );
+}
+
+const readConfig = ( ) => {
+    config = JSON.parse( fs.readFileSync( globalConfigPath, 'utf8' ) );
+}
+
+if ( !fs.existsSync( globalConfigPath ) ) {
+    writeConfig( );
+}
+else {
+    readConfig( );
+}
+
+exports.getConfig = ( ) => {
+    return config;
+}
+
+exports.set = ( name, value ) => {
+    config[ name ] = value;
+
+    writeConfig( );
+};
+
+exports.get = ( name ) => {
+    return config[ name ];
+};
+
+exports.clean = ( ) => {
+    config = defaultConfig;
+
+    writeConfig( );
+};
